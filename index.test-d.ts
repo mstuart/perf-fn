4be52e8bf7699156e7eb81a0395058f5d645f133
Mark Stuart <mstuart@users.noreply.github.com>
2026-08-09 21:07:23 -0700

    chore: enforce ultracite

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,12 @@
-import {expectType, expectError} from 'tsd';
-import perfFn, {perfFnSync, type PerfResult} from './index.js';
+import { expectError, expectType } from "tsd";
+import perfFn, { type PerfResult, perfFnSync } from "./index.js";
 
 const asyncResult = await perfFn(async () => 42);
 expectType<PerfResult<number>>(asyncResult);
 expectType<number>(asyncResult.result);
 expectType<number>(asyncResult.duration);
 
-const syncResult = perfFnSync(() => 'hello');
+const syncResult = perfFnSync(() => "hello");
 expectType<PerfResult<string>>(syncResult);
 expectType<string>(syncResult.result);
 expectType<number>(syncResult.duration);
